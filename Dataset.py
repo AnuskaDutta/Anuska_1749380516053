c9a64757c450f6ee356f1b49cb73a0c6b81e8c57
AnuskaDutta <duttaanu2121@gmail.com>
2026-09-08 15:36:47 +0530

    Update ticket date range in Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -31,8 +31,8 @@ from datetime import datetime, timedelta
 # ----------------------------------------------------------------------------
 SEED = 42
 N_TICKETS = 4000
-START_DATE = datetime(2025, 1, 1)
-END_DATE = datetime(2025, 12, 31, 23, 59)
+START_DATE = datetime(2024, 07, 1)
+END_DATE = datetime(2025, 06, 29, 23, 59)
 
 rng = np.random.default_rng(SEED)
 
